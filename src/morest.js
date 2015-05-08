9cabc5cab0cd1052363ea3162cece8962d6a1282
Robin Franken <robin0165@hotmail.com>
2015-05-08 23:45:11 +0200

    updated Morest to use a mongoose instance

--- src/morest.js
+++ src/morest.js
@@ -1,11 +1,9 @@
-var mongoose = require('mongoose');
-
 var Controller = function (opts) {
     //Parse all the options
     this.availableOperations = opts.availableOperations || ['GET_ALL', 'GET', 'POST', 'PUT', 'DELETE'];
     this.schema = opts.schema || null;
 
-    this._mongooseModel = mongoose.model(this.schema);
+    //this._mongooseModel = mongoose.model(this.schema);
 };
 
 Controller.prototype = {
@@ -88,53 +86,62 @@ Controller.prototype = {
 
     getBaseRoute: function () {
         return this._mongooseModel.collection.name.toLowerCase();
+    },
+
+    setMongooseInstance: function(mongoose){
+        this._mongooseModel = mongoose.model(this.schema);
     }
 };
 
 module.exports.Controller = Controller;
 
-var Morest = function (router, opts) {
-    this.router = router; //Express router
+var Morest = function (router, mongoose, opts) {
     this.controllers = opts.controllers || []; //Define the controllers
     this.baseRoute = opts.baseRoute || '/'; //Define a base router from which all routes extend, e.g. '/api/'
 
-    if (this.router === null || typeof this.router === 'undefined') {
+    if (router === null || typeof router === 'undefined') {
         throw new Error('[Morest] Router not defined');
+    }
+
+    if (mongoose === null || typeof mongoose === 'undefined'){
+        throw new Error('[Morest] Mongoose not defined');
     }
 
     //Generate routes
     for (var c = 0; c < this.controllers.length; c++) {
         var controller = this.controllers[c];
 
-        this.router.all(this.baseRoute + controller.getBaseRoute(), controller.ALL.bind(controller));
+        controller.setMongooseInstance(mongoose);
+
+        router.all(this.baseRoute + controller.getBaseRoute(), controller.ALL.bind(controller));
 
         if (controller.availableOperations.indexOf('GET_ALL') > -1) {
             //GET_ALL
-            this.router.get(this.baseRoute + controller.getBaseRoute(), controller.GET_ALL.bind(controller));
+            router.get(this.baseRoute + controller.getBaseRoute(), controller.GET_ALL.bind(controller));
         }
 
         if (controller.availableOperations.indexOf('GET') > -1) {
             //GET
-            this.router.get(this.baseRoute + controller.getBaseRoute() + '/:id', controller.GET.bind(controller));
+            router.get(this.baseRoute + controller.getBaseRoute() + '/:id', controller.GET.bind(controller));
         }
 
         if (controller.availableOperations.indexOf('POST') > -1) {
             //POST
-            this.router.post(this.baseRoute + controller.getBaseRoute(), controller.POST.bind(controller));
+            router.post(this.baseRoute + controller.getBaseRoute(), controller.POST.bind(controller));
         }
 
         if (controller.availableOperations.indexOf('PUT') > -1) {
             //PUT
-            this.router.put(this.baseRoute + controller.getBaseRoute() + '/:id', controller.PUT.bind(controller));
+            router.put(this.baseRoute + controller.getBaseRoute() + '/:id', controller.PUT.bind(controller));
         }
 
         if (controller.availableOperations.indexOf('DELETE') > -1) {
             //DELETE
-            this.router.delete(this.baseRoute + controller.getBaseRoute() + '/:id', controller.DELETE.bind(controller));
+            router.delete(this.baseRoute + controller.getBaseRoute() + '/:id', controller.DELETE.bind(controller));
         }
     }
 
-    return this.router;
+    return router;
 };
 
 module.exports.Morest = Morest;
